41505f743527e4932eba2756ad859ed388ca1cab
walter (abb) <waltex@libero.it>
2017-08-14 15:42:13 +0200

    test

--- views/site/my_test.php
+++ views/site/my_test.php
@@ -1,6 +1,14 @@
 <?php
 echo 'kldkldjd';
 echo "làòklklkihjh dd";
+<<<<<<< HEAD
 
 
 echo 'akajjan';
+=======
+<<<<<<< HEAD
+echo "4";
+=======
+echo "3";
+>>>>>>> 5e6a727340d0ab480f81c8b9e4cc32046ab9370e
+>>>>>>> 542ecd6aa95f3fc8451232d1a60396f6caa47d2c
