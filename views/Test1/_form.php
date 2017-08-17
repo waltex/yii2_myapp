72d8f5986a77a85eb5fa49d920d3b46ea364efb1
walter (abb) <waltex@libero.it>
2017-08-17 18:11:12 +0200

    test yii

--- views/Test1/_form.php
+++ views/Test1/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+
+//use app\models\crud1\Test2;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\crud1\Test1 */
@@ -28,12 +31,29 @@ use yii\widgets\ActiveForm;
             ], ['prompt' => Yii::t('app', 'Select category')]);
     ?>
 
-<?= $form->field($model, 'campof')->textInput() ?>
+    <!--?=
+    //$result = app\models\crud1\Test2Crud::find()->asArray()->all();
+    //
+    //$users = [1, 2, 3]; //Users::find()->all();
+    //$users = Users::find()->all();
+    //$connection = \Yii::$app->db;
+//$model = $connection->createCommand('SELECT * FROM test2');
+//$result = $model->queryAll();
+//$items = ArrayHelper::map($result, 'id', 'nome');
+    //$form->field($model, 'id_test2')->textInput(['maxlength' => true])->dropdownList($items, ['prompt' => Yii::t('app', 'Select category')]);
+    ?-->
+
+
+
+
+
+
+    <?= $form->field($model, 'campof')->textInput() ?>
 
     <div class="form-group">
-                <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
-        <?php ActiveForm::end(); ?>
+    <?php ActiveForm::end(); ?>
 
 </div>
