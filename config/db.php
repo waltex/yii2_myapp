b3b1696e4b255007dd8fd7d5179b22bae838717a
Walter Cardelli (imac) <waltex@libero.it>
2017-08-22 20:10:24 +0200

    test yii

--- config/db.php
+++ config/db.php
@@ -1,9 +1,28 @@
 <?php
-
+/*
+  return [
+  'class' => 'yii\db\Connection',
+  'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+  'username' => 'root',
+  'password' => '',
+  'charset' => 'utf8',
+  ];
+ */
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'db2' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+    ],
 ];
