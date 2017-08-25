238d65d9d56686983fb9545c38aa4e2f8234192b
walter (abb) <waltex@libero.it>
2017-08-25 15:28:46 +0200

    test yii

--- views/site/hello.php
+++ views/site/hello.php
@@ -2,3 +2,22 @@
 <?= $name."kjhkjh"."<br>"; ?>
 <?php  echo "my name: ".  $name ."<br>"; ?>
 <?= $name ?>
+
+<?php
+
+use yii\bootstrap\Alert;
+use yii\bootstrap\Button;
+
+echo Alert::widget([
+    'options' => [
+    'class' => 'alert-danger',
+    ],
+    'body' => 'Say hello...',
+    'closeButton' => false
+]);
+
+echo Button::widget([
+    'label' => 'Action',
+    'options' => ['class' => 'btn-lg'],
+]);
+?>
